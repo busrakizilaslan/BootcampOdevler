fbfe6086c23ecee338a59918e86cfd7c09338da1
Busra Kizilaslan Yalcin <busra_kizilaslan@hotmail.com>
2022-06-16 14:32:28 +0300

    odev2 completed

--- app/src/main/java/com/busrayalcin/bootcampodevler/odevler/Odev2.kt
+++ app/src/main/java/com/busrayalcin/bootcampodevler/odevler/Odev2.kt
@@ -1,4 +1,4 @@
-package com.busrayalcin.bootcampodevler.odev2
+package com.busrayalcin.bootcampodevler.odevler
 
 class Odev2 {
 
@@ -7,10 +7,12 @@ class Odev2 {
         return sicaklikDerece * 1.8 + 32
     }
 
+
     //Kenarları parametre olarak girilen dikdörtgenin çevresini hesaplayan metot
     fun soru2(kisaKenar:Double, uzunKenar:Double) : Double {
         return 2 * (kisaKenar + uzunKenar)
     }
+
 
     //Girilen değerin faktöriyelini hesaplayan metot
     fun soru3(sayi:Int) : Int {
@@ -24,6 +26,7 @@ class Odev2 {
         return faktoriyel
     }
 
+
     //Girilen kelime ve harf için harfin kelime içinde kaç adet olduğunu gösteren metot
     fun soru4(kelime:String, harf:Char) : Int {
         var sayac = 0
@@ -36,6 +39,32 @@ class Odev2 {
     }
 
 
+    //Girilen kenar sayısına göre iç açılar toplamını hesaplayan metot
+    fun soru5(kenarSayisi:Int) : Int {
+        return (kenarSayisi - 2) * 180
+    }
 
+
+    //Girilen gün sayısına göre maaş hesabı yapan metot
+    fun soru6(gun:Int) : Int {
+        val calismaSaati = 8
+        var maas = 0
+        if (gun > 20){
+            maas = 1600 + (gun - 20) * 20 * calismaSaati
+        }else{
+            maas = gun * 10 * calismaSaati
+        }
+        return maas
+    }
+
+
+    //Girilen kota değerine göre ücreti hesaplayan metot
+    fun soru7(kota:Int) : Int {
+        var ucret = 100
+        if (kota > 50){
+            ucret += (kota - 50) * 4
+        }
+        return ucret
+    }
 
 }
--- app/src/main/java/com/busrayalcin/bootcampodevler/odevler/Odev2Main.kt
+++ app/src/main/java/com/busrayalcin/bootcampodevler/odevler/Odev2Main.kt
@@ -1,24 +1,21 @@
-package com.busrayalcin.bootcampodevler.odev2
+package com.busrayalcin.bootcampodevler.odevler
 
 fun main() {
     var odev2 = Odev2()
     var sicaklikDerece = 44.4
     var sicaklikFahrenheit = odev2.soru1(sicaklikDerece)
     println("Sıcaklık Derece : $sicaklikDerece Fahrenheit : $sicaklikFahrenheit")
-
     println("--------------------------------------------")
 
 
     var cevre = odev2.soru2(17.17,44.44)
     println("Dikdörtgenin Çevresi : $cevre")
-
     println("--------------------------------------------")
 
 
     var sayi = 5
     var faktoriyel = odev2.soru3(sayi)
     println("Girilen Sayı : $sayi  Faktöriyel Değeri : $faktoriyel")
-
     println("--------------------------------------------")
 
 
@@ -26,12 +23,23 @@ fun main() {
     var harf = 'a'
     var sonuc = odev2.soru4(kelime,harf)
     println("Girilen Kelime : $kelime  Aranan Harf : $harf  Adet : $sonuc")
-
     println("--------------------------------------------")
 
 
+    var kenarSayisi = 5
+    var cikti = odev2.soru5(kenarSayisi)
+    println("Kenar Sayısı : $kenarSayisi  İç Açılar Toplamı : $cikti")
+    println("--------------------------------------------")
 
 
+    var gun = 21
+    var maas = odev2.soru6(gun)
+    println("Girilen Gün Sayısı : $gun  Maaş Tutarı : $maas")
+    println("--------------------------------------------")
 
 
+    var kota = 52
+    var ucret = odev2.soru7(kota)
+    println("Kota : $kota  Ücret : $ucret")
+    println("--------------------------------------------")
 }
